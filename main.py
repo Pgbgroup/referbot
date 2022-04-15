82a88f0a47246f41122075fde7b3240e605d84a5
Pgbgroup <80152512+Pgbgroup@users.noreply.github.com>
2022-04-15 14:23:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -323,8 +323,8 @@ def amo_with(message):
     json.dump(data, open('users.json', 'w'))
     bot.send_message(user_id, "✅* Withdraw is request to our owner automatically\n\n💹 Payment Channel :- "+PAYMENT_CHANNEL +"*", parse_mode="Markdown")
     
-     send = bot.send_message(PAYMENT_CHANNEL,  "✅* New Withdraw\n\n⭐ Amount - "+str(amo)+f" {TOKEN}\n🦁 User - @"+message.from_user.username+"\n💠 Wallet* - `"+data['wallet'][user]+"`\n☎️ *User Referrals = "+str(
-        data['referred'][user])+"\n\n🏖 Bot Link - @"+bot_name+"\n\n#UsersRequest*", parse_mode="Markdown")
+     send = bot.send_message(OWNER_ID,  "✅* New Withdraw\n\n⭐ Amount - "+str(amo)+f" {TOKEN}\n🦁 User - @"+message.from_user.username+"\n💠 Wallet* - `"+data['wallet'][user]+"`\n☎️ *User Referrals = "+str(
+        data['referred'][user])+"\n\n🏖 Bot Link - @"+bot_name+"\n\n#UsersRequest*",parse_mode="Markdown")
             
    except:
         bot.send_message(message.chat.id, "This command having error pls wait for ficing the glitch by admin")
